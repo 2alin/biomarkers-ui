6bf810f3b81523998b225dff09a82ee8f95e8c86
Adilson Sandoval <adilsonsdev@gmail.com>
2026-01-28 19:31:15 +0100

    add improved loading page

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,13 +1,15 @@
 const parsedUrl = new URL(window.location.href);
 const { searchParams } = parsedUrl;
 
+const delayInSeconds = Number(searchParams.get("secondsDelay"));
+
 export default {
   // Place where we have our biomarkers data
   biomarkersDataPath: "/data/biomarkers.json",
   // Place where we have our results data
   resultsDataPath: "/data/results.json",
   // Delay (in ms) to simulate real scenario
-  responseDelay: 2000,
+  responseDelay: delayInSeconds ? delayInSeconds * 1000 : 2000,
   // Whether we should simulate an error in the API or not,
   simulateError: searchParams.has("simulateError") || false,
 };
--- src/sections/LoadingSection.tsx
+++ src/sections/LoadingSection.tsx
@@ -1,7 +1,11 @@
 export default function LoadingSection() {
   return (
-    <section>
-      <p>Loading Section</p>
+    <section className="flex flex-1 flex-col items-center justify-center p-2 font-semibold text-secondary-600">
+      <header className="mb-2 flex flex-col items-center gap-2 text-lg font-bold text-secondary-800">
+        <span className="size-20 bg-primary-500 mask-[url(./assets/loader-circle.svg)] mask-size-[100%] motion-safe:animate-spin"></span>
+        <h1>Loading data</h1>
+      </header>
+      <p>Please wait.</p>
     </section>
   );
 }
